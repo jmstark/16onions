993e83762196182b1d49db400c3a4d760aeb35bc
Sree Harsha Totakura <sreeharsha@totakura.in>
2015-08-26 10:45:33 +0200

    Fix worker thread execution starting point.
    Pooled worker threads currently start executing at the listener object's run()
    causing bind exception.  This is incorrect as they have to execute the run()
    method of the server implementation.  This commit fixes it.

--- src/tools/Server.java
+++ src/tools/Server.java
@@ -14,32 +14,41 @@ import java.util.concurrent.Executors;
  *
  * @author Emertat
  */
-public class Server implements Runnable {
+public abstract class Server implements Runnable {
 
     protected static ArrayList<Socket> socketBuffer;
 
-    @Override
-    public void run() {
+    private class Listener implements Runnable {
+
+        private Server serverImpl;
+        private int port;
+        private ExecutorService clientProcessingPool;
+
+        Listener(Server serverImpl, final int port) {
+            this.serverImpl = serverImpl;
+            this.port = port;
+            this.clientProcessingPool = Executors.newFixedThreadPool(10);
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.println("Going to start a server with port: " + this.port);
+                ServerSocket serverSocket = new ServerSocket(this.port);
+                while (true) {
+                    Socket clientSocket = serverSocket.accept();
+                    socketBuffer.add(clientSocket);
+                    clientProcessingPool.submit(this.serverImpl);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     protected void startServer(final int port) {
-        final ExecutorService clientProcessingPool = Executors.newFixedThreadPool(10);
-        Runnable serverTask = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    ServerSocket serverSocket = new ServerSocket(port);
-                    while (true) {
-                        Socket clientSocket = serverSocket.accept();
-                        socketBuffer.add(clientSocket);
-                        clientProcessingPool.submit(this);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        Thread serverThread = new Thread(serverTask);
-        serverThread.start();
+        Listener listener = new Listener(this, port);
+        Thread listenThread = new Thread(listener);
+        listenThread.start();
     }
 }
