e6356728cf61c5791783437cc2e544971755183d
Sree Harsha Totakura <sreeharsha@totakura.in>
2016-09-20 14:48:15 +0200

    add script to start modules of a peer

---
+++ contrib/start.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+import subprocess as proc
+import sys
+import time
+import os
+import signal
+
+JAVA_PATH = "java"
+
+MODULES = ["gossip.Main",
+           "mockups.rps.Main",
+           "mockups.nse.Main",
+           "mockups.auth.Main"]
+PROCESSES = []
+
+#Flag used for signalling termination
+TERMINATE = False
+
+def start_module(name, args):
+    split_index = -2
+    java_args = args[:split_index]
+    java_args.append(name)
+    java_args.extend(args[split_index:])
+    print(java_args)
+    return proc.Popen(args=java_args, executable=JAVA_PATH, cwd=os.getcwd())
+
+def start_all():
+    global MODULES
+    global PROCESSES
+    for module in MODULES:
+        PROCESSES.append(start_module(module,args))
+
+def terminate_all():
+    for process in PROCESSES:
+        process.terminate()
+    for process in PROCESSES:
+        process.wait()
+    ret = 0;
+    for process in PROCESSES:
+        if 0 != process.returncode:
+            ret = process.returncode
+    return ret
+
+def signal_cb_sigchld(signum, frame):
+    global TERMINATE
+    signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+    TERMINATE = True
+
+def signal_cb_sigterm(signum, frame):
+    global TERMINATE
+    TERMINATE = True
+
+def setup_signals():
+    signal.signal(signal.SIGCHLD, signal_cb_sigchld)
+    signal.signal(signal.SIGTERM, signal_cb_sigterm)
+    signal.signal(signal.SIGINT, signal_cb_sigterm)
+
+def await():
+    global TERMINATE
+    while not TERMINATE:
+        signal.pause()
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    setup_signals()
+    start_all()
+    await()
+    exit(terminate_all())
+
+    
+
+
