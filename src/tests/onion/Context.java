39aa75b3984ee9b438bc8df0597a69d69a17ab1e
Sree Harsha Totakura <sreeharsha@totakura.in>
2016-10-06 13:15:49 +0200

    Add onion tester

---
+++ src/tests/onion/Context.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2016 Sree Harsha Totakura <sreeharsha@totakura.in>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package tests.onion;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.security.interfaces.RSAPublicKey;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import onion.api.OnionErrorMessage;
+import onion.api.OnionTunnelBuildMessage;
+import onion.api.OnionTunnelDataMessage;
+import onion.api.OnionTunnelIncomingMessage;
+import onion.api.OnionTunnelReadyMessage;
+import protocol.Connection;
+import protocol.MessageHandler;
+import protocol.MessageParserException;
+import protocol.MessageSizeExceededException;
+import protocol.Protocol;
+import protocol.ProtocolException;
+
+/**
+ *
+ * @author Sree Harsha Totakura <sreeharsha@totakura.in>
+ */
+class Context extends MessageHandler<Void> {
+
+    private final InetSocketAddress targetAddress;
+    private final RSAPublicKey targetHostkey;
+    private final Connection connection;
+    private final Logger logger;
+
+    enum State {
+        CREATE_TUNNEL, TUNNEL_CREATED,
+    };
+
+    private State state;
+
+    Context(Connection connection, RSAPublicKey targetHostkey,
+            InetSocketAddress targetAddress, Logger logger) {
+        super(null);
+        this.connection = connection;
+        this.targetHostkey = targetHostkey;
+        this.targetAddress = targetAddress;
+        this.logger = logger;
+    }
+
+    void ready() {
+        this.connection.receive(this);
+        state = State.CREATE_TUNNEL;
+    }
+
+    private void createTunnel() {
+        OnionTunnelBuildMessage onionTunnelBuildMessage;
+        try {
+            onionTunnelBuildMessage = new OnionTunnelBuildMessage(targetAddress,
+                    targetHostkey);
+        } catch (MessageSizeExceededException ex) {
+            throw new RuntimeException("Message size exceeded");
+        }
+        connection.sendMsg(onionTunnelBuildMessage);
+    }
+
+    /**
+     * Send data on the tunnel with the given ID
+     *
+     * @param id the tunnel ID to send the data on
+     */
+    private void sendData(long id, String actual) {
+        OnionTunnelDataMessage message;
+        try {
+            message = new OnionTunnelDataMessage(id, actual.getBytes());
+        } catch (MessageSizeExceededException ex) {
+            throw new RuntimeException();
+        }
+        connection.sendMsg(message);
+    }
+
+    @Override
+    public void parseMessage(ByteBuffer buf, Protocol.MessageType type,
+            Void closure) throws MessageParserException, ProtocolException {
+        switch (type) {
+            case API_ONION_TUNNEL_READY:
+                if (State.CREATE_TUNNEL != state) {
+                    throw new ProtocolException(
+                            "We did not ask to create a tunnel");
+                }
+                 {
+                    state = State.TUNNEL_CREATED;
+                OnionTunnelReadyMessage ready;
+                ready = OnionTunnelReadyMessage.parse(buf);
+                long id = ready.getId();
+                     sendData(id, "Hello World; it's a classical message");
+                }
+                return;
+            case API_ONION_TUNNEL_INCOMING:
+            {
+                OnionTunnelIncomingMessage message;
+                message = OnionTunnelIncomingMessage.parser(buf);
+                byte[] otherHostkeyBytes = message.getKeyEncoding();
+                logger.fine("Received a new incoming tunnel connection");
+            }
+                return;
+            case API_ONION_TUNNEL_DATA:
+            {
+                OnionTunnelDataMessage message;
+                message = OnionTunnelDataMessage.parse(buf);
+                String actual = new String(message.getData());
+                logger.log(Level.FINE, "Received data from Tunnel {0}: {1}",
+                        new Object[]{message.getId(), actual});
+                sendData(message.getId(), "ACK back to you");
+            }
+            return;
+            case API_ONION_ERROR:
+            {
+                OnionErrorMessage message;
+                message = OnionErrorMessage.parser(buf);
+                logger.log(Level.WARNING,
+                        "Onion error on tunnel {0}while serving request of type: {1}",
+                        new Object[]{message.getId(), message.
+                            getRequestType().name()});
+            }
+                return;
+            default:
+                throw new ProtocolException(
+                        "Received unknown message type " + type.name());
+        }
+    }
+
+}
---
+++ src/tests/onion/Main.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2016 Sree Harsha Totakura <sreeharsha@totakura.in>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package tests.onion;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.security.InvalidKeyException;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import nse.api.QueryMessage;
+import onion.OnionConfiguration;
+import onion.OnionConfigurationImpl;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import protocol.Connection;
+import protocol.DisconnectHandler;
+import util.PEMParser;
+import util.Program;
+import util.config.CliParser;
+
+/**
+ *
+ * @author Sree Harsha Totakura <sreeharsha@totakura.in>
+ */
+public class Main extends Program {
+
+    private InetSocketAddress api_address;
+    private InetSocketAddress targetAddress;
+    private Connection connection;
+    private Context context;
+    private RSAPublicKey targetHostkey;
+
+    Main() {
+        super("tests.onion", "API conformance test case for ONION");
+    }
+
+    Logger getLogger() {
+        return this.logger;
+    }
+
+    @Override
+    protected void addParserOptions(CliParser parser) {
+        // Add options for the target hostkey and target onion P2P address
+        parser.addOption(Option.builder("k")
+                .required(true)
+                .longOpt("hostkey")
+                .desc("Target hostkey")
+                .optionalArg(false)
+                .argName("FILE")
+                .hasArg().build());
+        parser.addOption(Option.builder("t")
+                .required(true)
+                .longOpt("address")
+                .desc("Target ONION P2P address")
+                .optionalArg(false)
+                .argName("IP ADDRESS")
+                .hasArg().build());
+        parser.addOption(Option.builder("p")
+                .required(true)
+                .longOpt("address")
+                .desc("Target ONION P2P address port")
+                .optionalArg(false)
+                .argName("PORT")
+                .hasArg().build());
+    }
+
+    @Override
+    protected void parseCommandLine(CommandLine cli, CliParser parser) {
+        String filename = parser.getConfigFilename("onion.conf");
+        OnionConfiguration config;
+        try {
+            config = new OnionConfigurationImpl(filename);
+        } catch (IOException ex) {
+            throw new RuntimeException("Unable to read config file");
+        }
+        api_address = config.getAPIAddress();
+        //retrieve the target hostkey
+        String target_hostkey = cli.getOptionValue("k");
+        String hostname = cli.getOptionValue("t");
+        String port = cli.getOptionValue("p");
+        targetAddress = new InetSocketAddress(hostname, Integer.parseInt(port));
+        try {
+            targetHostkey = PEMParser.getPublicKeyFromPEM(new File(
+                    target_hostkey));
+        } catch (IOException | InvalidKeyException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    protected void cleanup() {
+        if (null != connection) {
+            connection.disconnect();
+            connection = null;
+        }
+    }
+
+    @Override
+    protected void run() {
+        AsynchronousSocketChannel channel;
+        try {
+            channel = AsynchronousSocketChannel.open(this.group);
+        } catch (IOException ex) {
+            throw new RuntimeException();
+        }
+        channel.connect(api_address, channel, new ConnectCompletion());
+    }
+
+    private class ConnectCompletion
+            implements CompletionHandler<Void, AsynchronousSocketChannel> {
+
+        private final Random random;
+
+        private ConnectCompletion() {
+            this.random = new Random();
+        }
+
+        @Override
+        public void completed(Void arg0, AsynchronousSocketChannel channel) {
+            connection = new Connection(channel, new DisconnectHandler(null) {
+                @Override
+                protected void handleDisconnect(Object closure) {
+                    if (!tests.onion.Main.this.inShutdown()) {
+                        logger.log(Level.WARNING, "Connection disconnected");
+                        connection = null;
+                        shutdown();
+                    }
+                }
+            });
+            context = new Context(connection, targetHostkey, targetAddress,
+                    Main.this.logger);
+            context.ready();
+        }
+
+        @Override
+        public void failed(Throwable arg0, AsynchronousSocketChannel arg1) {
+            logger.log(Level.SEVERE, "Cannot connect to Onion API");
+            shutdown();
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        new Main().start(args);
+    }
+}
