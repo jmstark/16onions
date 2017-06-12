9a65b8f3410bd1ce890225d04271e8ebdb6a8a71
Josef Stark <j.stark@tum.de>
2017-06-13 00:10:11 +0200

    Added message forwarding logic

--- src/com/voidphone/onion/OnionBaseSocket.java
+++ src/com/voidphone/onion/OnionBaseSocket.java
@@ -5,6 +5,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
@@ -20,7 +22,18 @@ public abstract class OnionBaseSocket
 	public final static int MAGIC_SEQ_CONNECTION_START = 0x7af3bef1;
 	public final static int VERSION = 1;
 	
+	protected final byte MSG_BUILD_TUNNEL = 0xb;
+	protected final byte MSG_DESTROY_TUNNEL = 0xd;
+
+	protected final int MAX_DATA_PACKET_SIZE = 65536/2;
 	protected static APIOnionAuthSocket oasock = null;
+	protected int tunnelId;
+	protected DatagramSocket nextHopDataOutgoing = null;
+	protected DatagramSocket lastHopDataOutgoing = null;	
+	protected byte[] nextHopAddress = null;
+	protected short nextHopPort = 0;
+	protected byte[] lastHopAddress = null;
+	protected short lastHopPort = 0;
 
 	abstract void initiateOnionConnection(DataInputStream in, DataOutputStream out, byte hostkey[], int bufferSize)
 			throws IOException;
@@ -41,6 +54,42 @@ public abstract class OnionBaseSocket
 		this(sock.getDataInputStream(), sock.getDataOutputStream(), hostkey, bufferSize);
 	}
 
+	
+	/**
+	 * Processes the next UDP data message or forwards it,
+	 * if there is a next hop. Since a node can have more
+	 * than one tunnel through it, a dispatcher receives
+	 * all incoming UDP packets, extracts the tunnel ID
+	 * and then calls this method of the according instance.
+	 * @param in
+	 * @throws IOException 
+	 */
+	void processNextDataMessage(DatagramPacket incomingPacket) throws IOException
+	{
+		DatagramSocket forwardTarget = null;
+		if(incomingPacket.getAddress().getAddress().equals(lastHopAddress) && incomingPacket.getPort() == lastHopPort)
+		{
+			forwardTarget = nextHopDataOutgoing;
+		}
+		else if(incomingPacket.getAddress().getAddress().equals(nextHopAddress) && incomingPacket.getPort() == nextHopPort)
+		{
+			forwardTarget = lastHopDataOutgoing;
+		}
+		
+		if(forwardTarget == null)
+		{
+			//no forwarding, this packet is for us
+			//TODO: forward data to API
+		}
+		else
+		{
+			//forward data to next node
+			forwardTarget.send(incomingPacket);
+		}
+
+	}
+	
+	
 	public DataInputStream getDataInputStream()
 	{
 		return null;
--- src/com/voidphone/onion/OnionListenerSocket.java
+++ src/com/voidphone/onion/OnionListenerSocket.java
@@ -7,6 +7,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -15,93 +17,110 @@ import sun.net.InetAddressCachePolicy;
 import com.voidphone.api.APIOnionAuthSocket;
 import com.voidphone.api.APIOnionAuthSocket.AUTHSESSIONINCOMINGHS1;
 
-/** Main application runs a TCP server socket. There, when it receives a 
- * new connection, it then constructs an OnionServerSocket, passing the new 
- * TCP socket, the remote hostkey and the buffersize (how to determine?)
- *  to the constructor. Then it should send a TUNNEL_READY API message.
- *  
+/**
+ * Main application runs a TCP server socket. There, when it receives a new
+ * connection, it then constructs an OnionServerSocket, passing the new TCP
+ * socket, the remote hostkey and the buffersize (how to determine?) to the
+ * constructor. Then it should send a TUNNEL_READY API message.
+ * 
  */
 public class OnionListenerSocket extends OnionBaseSocket
 {
 
-	private byte[] ownAddress = {0,0,0,0};
-	private byte[] nextHopAddress = null;
-	byte nextHopPort = 0;
-	private DataOutputStream nextHopControlMsgOut = null;
+	private byte[] ownAddress = { 0, 0, 0, 0 };
+	private short ownPort = 0;
+	private DataOutputStream nextHopControlMsgOutgoing = null;
+	private DataInputStream lastHopControlMsgIncoming = null;
+	private Socket nextHopSocket = null;
+	protected static DatagramSocket dataIncoming;// = new DatagramSocket(1423);
+
 
 	public OnionListenerSocket(Socket sock, byte[] hostkey, int size) throws IOException
 	{
 		super(sock, hostkey, size);
 	}
-	
-	
+
 	/**
-	 * Function handling TCP control messages. 
-	 * Those are used for building tunnels.
-	 * @param in
-	 * @param out
-	 * @throws IOException 
+	 * This function is used to build and destroy tunnels. Building/destroying
+	 * is done iteratively. A control message only consists of message type,
+	 * address length (4 or 6, depending on IP version), IP address and port
+	 * number (same port number for TCP and UDP). The function receives the
+	 * message, and if the next hop is already known the message gets forwarded
+	 * there, if not, the next hop is constructed.
+	 * 
+	 * @throws IOException
 	 */
-	void processNextControlMessage(DataInputStream in, DataOutputStream out) throws Exception
+	void processNextControlMessage() throws Exception
 	{
-		byte recipientAddressLength = in.readByte();
-		byte[] recipient = new byte[recipientAddressLength];
-		in.readFully(recipient);
-		byte recipientPort = in.readByte();
-		if(recipient.equals(ownAddress))
+		byte messageType = lastHopControlMsgIncoming.readByte();
+		if (messageType == MSG_DESTROY_TUNNEL)
 		{
-			if(nextHopAddress != null)
-				throw new Exception("nextHop already defined; don't know what to do");
-			//this can only mean that we must add a new hop
-			//since there are no other control messages.
-			byte nextHopAddressLength = in.readByte();
-			nextHopAddress = new byte[nextHopAddressLength];
-			in.readFully(nextHopAddress);	
-			nextHopPort = in.readByte();
-			Socket nextHopSocket = new Socket(InetAddress.getByAddress(nextHopAddress),nextHopPort);
-			nextHopControlMsgOut = new DataOutputStream(nextHopSocket.getOutputStream());
+			//forward message
+			nextHopControlMsgOutgoing.write(messageType);
+			
+			// tear down tunnel
+			nextHopControlMsgOutgoing.close();
+			nextHopSocket = null;
+			nextHopControlMsgOutgoing = null;
+			nextHopAddress = null;
+			nextHopPort = 0;
+			nextHopDataOutgoing.close();
+
+			lastHopAddress = null;
+			lastHopPort = 0;
+			lastHopDataOutgoing.close();
+		}
+		// construct next hop
+		byte destinationAddressLength = lastHopControlMsgIncoming.readByte();
+		byte[] destinationAddress = new byte[destinationAddressLength];
+		lastHopControlMsgIncoming.readFully(destinationAddress);
+		short destinationPort = lastHopControlMsgIncoming.readShort();
+
+		if (nextHopSocket != null)
+		{
+			// forward data to next hop
+			nextHopControlMsgOutgoing.write(messageType);
+			nextHopControlMsgOutgoing.write(destinationAddressLength);
+			nextHopControlMsgOutgoing.write(destinationAddress);
+			nextHopControlMsgOutgoing.write(destinationPort);
+
+
 		}
 		else
-		{
-			//forward the message to next hop
-			nextHopControlMsgOut.write(recipientAddressLength);
-			nextHopControlMsgOut.write()
-		}
-		
-		
+			if (messageType == MSG_BUILD_TUNNEL)
+			{
+				// connect to next hop
+				nextHopSocket = new Socket(InetAddress.getByAddress(destinationAddress), destinationPort);
+				nextHopControlMsgOutgoing = new DataOutputStream(nextHopSocket.getOutputStream());
+				nextHopAddress = destinationAddress.clone();
+				nextHopPort = destinationPort;
+				nextHopDataOutgoing.connect(InetAddress.getByAddress(nextHopAddress), nextHopPort);
+
+			}
+
 	}
-	
-	
-	void processNextDataMessage(DataInputStream in, DataOutputStream out)
-	{
-		
-	}
-	
-	
 
 	@Override
 	void initiateOnionConnection(DataInputStream in, DataOutputStream out, byte[] hostkey, int size) throws IOException
 	{
 
-		
 		APIOnionAuthSocket.AUTHSESSIONHS2 hs2;
 		byte[] buffer = new byte[size];
-		
-		if(in.readInt() != MAGIC_SEQ_CONNECTION_START || in.readInt() != VERSION)
+
+		if (in.readInt() != MAGIC_SEQ_CONNECTION_START || in.readInt() != VERSION)
 			throw new IOException("Tried to connect with non-onion node or wrong version node");
-		
-		
-		//read incoming hs1 from remote peer into buffer
-		//TODO: replace 1234 with actual hs1 length
+
+		// read incoming hs1 from remote peer into buffer
+		// TODO: replace 1234 with actual hs1 length
 		in.readFully(buffer, 0, 1234);
-		
-		//get hs2 from onionAuth and send it back to remote peer
-		//the API reply also contains a requestID and a sessionID.
-		//requestID is implicitely handled but maybe we have to save
-		//the session ID?
+
+		// get hs2 from onionAuth and send it back to remote peer
+		// the API reply also contains a requestID and a sessionID.
+		// requestID is implicitely handled but maybe we have to save
+		// the session ID?
 		hs2 = oasock.AUTHSESSIONINCOMINGHS1(new AUTHSESSIONINCOMINGHS1(hostkey, buffer));
 		out.write(hs2.getPayload());
-		
+
 	}
 
 }
