485e0fdb3140c470cc98b5d0f91937e6e500aa8f
jmstark <j.stark@tum.de>
2017-06-12 15:51:43 +0200

    tunnel building initial experimental code fragment

--- src/com/voidphone/onion/OnionListenerSocket.java
+++ src/com/voidphone/onion/OnionListenerSocket.java
@@ -7,7 +7,10 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
+
+import sun.net.InetAddressCachePolicy;
 
 import com.voidphone.api.APIOnionAuthSocket;
 import com.voidphone.api.APIOnionAuthSocket.AUTHSESSIONINCOMINGHS1;
@@ -21,6 +24,10 @@ import com.voidphone.api.APIOnionAuthSocket.AUTHSESSIONINCOMINGHS1;
 public class OnionListenerSocket extends OnionBaseSocket
 {
 
+	private byte[] ownAddress = {0,0,0,0};
+	private byte[] nextHopAddress = null;
+	byte nextHopPort = 0;
+	private DataOutputStream nextHopControlMsgOut = null;
 
 	public OnionListenerSocket(Socket sock, byte[] hostkey, int size) throws IOException
 	{
@@ -29,11 +36,46 @@ public class OnionListenerSocket extends OnionBaseSocket
 	
 	
 	/**
-	 * Endless loop fetching control messages (TCP)
-	 * E.g. forwarding instruction, tunnel teardown
+	 * Function handling TCP control messages. 
+	 * Those are used for building tunnels.
+	 * @param in
+	 * @param out
+	 * @throws IOException 
 	 */
-	//TODO
+	void processNextControlMessage(DataInputStream in, DataOutputStream out) throws Exception
+	{
+		byte recipientAddressLength = in.readByte();
+		byte[] recipient = new byte[recipientAddressLength];
+		in.readFully(recipient);
+		byte recipientPort = in.readByte();
+		if(recipient.equals(ownAddress))
+		{
+			if(nextHopAddress != null)
+				throw new Exception("nextHop already defined; don't know what to do");
+			//this can only mean that we must add a new hop
+			//since there are no other control messages.
+			byte nextHopAddressLength = in.readByte();
+			nextHopAddress = new byte[nextHopAddressLength];
+			in.readFully(nextHopAddress);	
+			nextHopPort = in.readByte();
+			Socket nextHopSocket = new Socket(InetAddress.getByAddress(nextHopAddress),nextHopPort);
+			nextHopControlMsgOut = new DataOutputStream(nextHopSocket.getOutputStream());
+		}
+		else
+		{
+			//forward the message to next hop
+			nextHopControlMsgOut.write(recipientAddressLength);
+			nextHopControlMsgOut.write()
+		}
+		
+		
+	}
 	
+	
+	void processNextDataMessage(DataInputStream in, DataOutputStream out)
+	{
+		
+	}
 	
 	
 
