37937970a7ebdd0b8678102f99544ce02c5e2774
Charlie Groh <ga58taw@mytum.de>
2017-07-25 16:54:25 +0200

    Add RPS-test

--- src/com/voidphone/testing/TesteePrintsAPIPort.java
+++ src/com/voidphone/testing/TesteePrintsAPIPort.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import com.voidphone.general.TestProcess;
 
 public class TesteePrintsAPIPort {
-	public static void main(String args[]) throws Exception{
+	public static void main(String args[]) throws Exception {
 		TestProcess p = new TestProcess(com.voidphone.onion.Main.class, new String[]{"-c", "bla"});
 		BufferedReader out = p.getOut();
 		Stub.getAPIPort(out);
